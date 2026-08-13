048a9cfe2213e944cd7d3c819e9efbc879e98f34
daviandresilva110610-cell <daviandresilva110610@gmail.com>
2026-08-13 11:31:35 -0300

    Add files via upload

---
+++ TPV/2ºBIMESTRE/Calculo/Calculo/FrmLogin.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculo
+{
+    public partial class FrmLogin : Form
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void checkboxseepass_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkboxseepass.Checked == true)
+            {
+                txtpassword.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                txtpassword.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+            if(txtusername.Text == "Davi" && txtpassword.Text == "123")
+            {
+                MessageBox.Show("Bem-vindo ao Sistema...");
+                Frmmenu menu = new Frmmenu();
+                menu.Show();
+                FrmLogin login = new FrmLogin();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Tente novamente");
+            }
+        }
+
+        private void btncancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ TPV/2ºBIMESTRE/Calculo/Calculo/FrmSplash.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculo
+{
+    public partial class FrmSplash : Form
+    {
+        public FrmSplash()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(progressBar1.Value < 100)
+            {
+                progressBar1.Value = progressBar1.Value + 1;
+                label1.Text = progressBar1.Value + "%";
+            }
+            else
+            {
+                //instanciamento do objeto form
+                //FrmSalarioBruto salarioBruto = new FrmSalarioBruto();
+                //salarioBruto.Show();
+                FrmLogin login = new FrmLogin();
+                login.Show();
+                //instanciamento do timer
+                timer1.Stop();
+
+                //esconder Splaah
+                this.Hide();
+            }
+        }
+    }
+}

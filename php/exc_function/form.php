810e57d63b256ee788e1b627a5ae20618d7557a0
daviandresilva110610-cell <daviandresilva110610@gmail.com>
2026-08-20 08:32:03 -0300

    Add files via upload

---
+++ php/exc_function/form.php
@@ -0,0 +1,52 @@
+<?php
+include 'funcoes.php';
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Cadastro de Aluno</title>
+</head>
+<body>
+
+<table border="1" align="center" width="49%">
+    <tr class="header">
+        <th>
+            CADASTRO DE ALUNO<br>
+            <?php data(); ?>
+        </th>
+    </tr>
+    <tr>
+        <td>
+        <br><br>
+            <form action="receber.php" method="post">
+
+                Nome do Aluno:
+                <input type="text" name="nome" >
+                <br><br>
+
+                Nota 1:
+                <input type="number" name="n1">
+                <br><br>
+
+                Nota 2:
+                <input type="number" name="n2">
+                <br><br>
+
+                <button type="submit">Enviar</button>
+
+                <br><br><input  type="reset" name="limpar" value="Limpar">
+            </form>
+        </td>
+    </tr>
+    <tr class="footer">
+        <td>Desenvolvido por: Davi Andre &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp &nbsp
+        <?php
+        exibirHoraSistema(); ?>
+        </td>
+    </tr>
+</table>
+
+</body>
+</html>
---
+++ php/exc_function/receber.php
@@ -0,0 +1,63 @@
+<?php
+include 'funcoes.php';
+
+function exibir($nome, $n1, $n2) {
+    print("Nome: $nome<br><br>");
+    print("Nota 1: $n1<br><br>");
+    print("Nota 2: $n2<br>");
+}
+
+function media($n1, $n2) {
+    $media = ($n1 + $n2) / 2;
+
+    if ($media >= 6 && $media <= 10) {
+        print("Média: $media<br>");
+        print("APROVADO!!!");
+    }
+    else {
+        print("Média: $media<br>");
+        print("<br>REPROVADO!!!");
+    }
+}
+
+$nome  = $_POST["nome"];
+$n1 = $_POST["n1"];
+$n2 = $_POST["n2"];
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Cadastro de Aluno</title>
+
+</head>
+<body>
+
+<table border="1" align="center" width="49%">
+    <tr class="header">
+        <th>
+            CADASTRO DE ALUNO<br>
+            <?php data(); ?>
+        </th>
+    </tr>
+    <tr>
+        <td>          
+            <?php
+            exibir($nome, $n1, $n2);
+            print("<br>");
+            media($n1, $n2);
+            ?>
+
+            <br>
+        </td>
+    </tr>
+    <tr class="footer">
+        <td>Desenvolvido por: Davi Andre &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp &nbsp
+            <?php exibirHoraSistema(); ?></td>
+    </tr>
+</table>
+
+</body>
+</html>
